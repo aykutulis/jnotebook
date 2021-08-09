ed3092c6f8750d854dd5e5b0538bcc1fb061d020
Aykut Ulış <aykutulis@hotmail.com>
2021-08-10 02:29:43 +0300

    cellReducer, immerjs

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,6 +1,8 @@
 import { ActionType } from '../actionTypes';
 import { Action } from '../actions';
 import { Cell } from './../Cell';
+import produce from 'immer';
+import { Reducer } from 'redux';
 
 interface CellsState {
   loading: boolean;
@@ -18,34 +20,56 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = (state: CellsState = initialState, action: Action): CellsState => {
+const reducer: Reducer<CellsState, Action> = produce((state = initialState, action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
-
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content: content,
-          },
-        },
-      };
+      state.data[id].content = content;
+      return;
 
     case ActionType.DELETE_CELL:
-      return state;
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+      return;
 
     case ActionType.MOVE_CELL:
-      return state;
+      const { direction } = action.payload;
+      const index = state.order.indexOf(action.payload.id);
+      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) return;
+
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+
+      return;
 
     case ActionType.INSERT_CELL_BEFORE:
-      return state;
+      const cell: Cell = {
+        content: '',
+        type: action.payload.type,
+        id: generateRandomId(),
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.indexOf(action.payload.id!);
+
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+
+      return;
 
     default:
       return state;
   }
+});
+
+const generateRandomId = () => {
+  return Math.random().toString(36).substr(2, 5);
 };
 
 export default reducer;
--- src/state/store.ts
+++ src/state/store.ts
@@ -2,4 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export const store = createStore(reducers, {}, applyMiddleware(thunk));
+const middlewares = [thunk];
+
+export const store = createStore(reducers, {}, applyMiddleware(...middlewares));
