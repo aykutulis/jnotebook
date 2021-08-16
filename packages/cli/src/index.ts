733bfb23b670133997e8dbaf449a5c4c8729fe5c
Aykut Ulış <aykutulis@hotmail.com>
2021-08-17 01:40:44 +0300

    cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,6 @@
-import serve from 'local-api';
+import { program } from 'commander';
+import { serveCommand } from './commands/serve';
 
-serve();
+program.addCommand(serveCommand);
+
+program.parse(process.argv);
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,3 +1,5 @@
-export default () => {
-  console.log('Server is listening..');
+export const serve = (port: number, filename: string, dir: string) => {
+  console.log('Serving traffic on port', port);
+  console.log('Saving/fetching cells from', filename);
+  console.log('That file is in dir', dir);
 };
