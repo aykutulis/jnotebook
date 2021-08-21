db6c2f0b3f8903543e417d16b34ef1457fc98d82
Aykut Ulış <aykutulis@hotmail.com>
2021-08-21 05:31:22 +0300

    before publish

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import { Command } from 'commander';
-import { serve } from 'local-api';
+import { serve } from '@jpen/local-api';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
